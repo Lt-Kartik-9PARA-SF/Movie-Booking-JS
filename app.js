add19017e5c6175aa160963db324fbffa262ef4f
Lt-Kartik-9PARA-SF <kartiktyagi22@gmail.com>
2022-11-08 15:12:27 +0530

    comiit

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
 import {fetchMovieAvailability,fetchMovieList} from "./api.js"
+
+ function working (event){
+    console.log(event);
+}
+
+let movieHolder = document.querySelector("#movie-holder");
+let data = fetchMovieList();
+data.then((response)=>{return response}).then((data)=>{
+    let str = "";
+    for(let i = 0; i < data.length;i++ ){
+        str = str+`<a class="movie- link" href="#moviename" >
+        <div class="movie" data- d="moviename" onclick="working(this)">
+        <div class="movie-img-wrapper">
+        <img src="${data[i].imgUrl}" alt = "logo" width="200px">
+        </div>
+        <h4 >${data[i].name}</h4>
+        </div>
+        </a>`;
+    }
+    movieHolder.innerHTML = str;
+});
+
